79a25294937049783fff984696bf7ae5a3616a80
Vojtech Kotek <kotek.vojtech@gmail.com>
2019-06-11 00:48:57 +0200

    Emails are now sent out individually with preferences from DB and a link to edit page with token

--- __main__.py
+++ __main__.py
@@ -19,7 +19,7 @@ class lunchScraper(object):
             }
         }
 
-    def add_menu(self, name, url, selector, n=0):
+    def add_menu(self, id, name, url, selector, n=0):
         try:
             with request.urlopen(url) as response:
                 html = response.read()
@@ -35,88 +35,104 @@ class lunchScraper(object):
                 else:
                     text = selected[n].get_text()
                 text = [t for t in text.split("\n") if len(t) > 0]
-            self.data['body']['menus'].append({'name':name,'url':url,'menu':text})
+            self.data['body']['menus'].append({'id':id,'name':name,'url':url,'menu':text})
         except Exception as e:
             print("Couldn't find menu for {}. Error: {}".format(name, e))
 
-    def send_message(self):
+    def send_messages(self):
+        
+        recipients = self.get_recipients()
+        
         auth = ("api", SETTINGS.MAIL_API_KEY)
-        data = {
-            "from": SETTINGS.FROM,
-            "to": self.get_recipients(SETTINGS.SUBSCRIBERS),
-            "subject": self.data['title'],
-            "h:X-Mailgun-Variables": json.dumps(self.data),
-            "t:text": "yes",
-            "template": "daily-menu",
-        }
-        r = requests.post(SETTINGS.MAIL_URL, auth=auth, data=data)
-        return r
+        results = {}
+        
+        for recipient in recipients:
+            menus = [r for r in self.data['body']['menus'] if str(r['id']) in recipient['preferences']]
+            data = { 'body': { 
+                'menus': menus,
+                'token': recipient['token'],
+            }}
+            config = {
+                "from": SETTINGS.FROM,
+                "to": recipient['email'],
+                "subject": self.data['title'],
+                "h:X-Mailgun-Variables": json.dumps(data),
+                "t:text": "yes",
+                "template": "daily-menu",
+            }
+            r = requests.post(SETTINGS.MAIL_URL, auth=auth, data=config)
+            
+        return True
 
-    def get_recipients(self, path_to_json):
-        with open(path_to_json, 'r') as f:
+    def get_recipients(self):
+        with open(SETTINGS.SUBSCRIBERS, 'r') as f:
             subscribers = json.load(f)
-            recipients = []
-            for subscriber in subscribers:
-                recipients.append(subscriber['email'])
-        return recipients
-    
+        return subscribers
+
 def clean(string):
     return "".join([char for char in string if char.isalnum() or char == " "])
 
 def your_restaurants(temp):
 
     # Pastva
+    id = 1
     name = "Pastva"
     url = "https://www.pastva-restaurant.cz/nase-menu/"
     selector = "#ffe_widget-2 .eff-panel-body"
-    temp.add_menu(name, url, selector)
+    temp.add_menu(id, name, url, selector)
 
     # Sodexo
+    id = 2
     name = "Sodexo, Riverview"
     url = "http://riverview.extranet.prod.dator3.cz/en/menu-for-the-week/"
     weekday = 4 - datetime.today().weekday()
     weekday = weekday if weekday > 0 else 0
     tag = "#menu-{} .popisJidla".format(str(weekday))
     selector = tag
-    temp.add_menu(name, url, selector, n=-1)
+    temp.add_menu(id, name, url, selector, n=-1)
 
     # FIVE - Weekly
+    id = 3
     name = "Dave B, Five (Week)"
     url = "https://www.daveb.cz/cs/denni-nabidka"
     selector = ".article .row div"
-    temp.add_menu(name, url, selector, n=1)
+    temp.add_menu(id, name, url, selector, n=1)
 
     # FIVE - Today
+    id = 3
     name = "Dave B, Five (Today)"
     url = "https://www.daveb.cz/cs/denni-nabidka"
     selector = ".article .row div"
     weekday = 3 + datetime.today().weekday()
     weekday = weekday if int(weekday) <= 7 else 7
-    temp.add_menu(name, url, selector, n=weekday)
+    temp.add_menu(id, name, url, selector, n=weekday)
 
     # Potrefena Husa - Na Verandach
+    id = 4
     name = "Potrefena Husa - Na Verandach"
     url = "https://www.phnaverandach.cz/"
     selector = ".listek-out .listek #table-1 .food-title"
-    temp.add_menu(name, url, selector, n=-1)
+    temp.add_menu(id, name, url, selector, n=-1)
 
     # Lavande Restaurant - Weekly
+    id = 5
     name = "Lavande Restaurant - Week"
     url = "https://restaurantlavande.cz/menu/#week-menu"
     selector = ".week-menu__header ~ .menus .menus__menu-content h3 ~ div .food__name"
-    temp.add_menu(name, url, selector, n=range(0,3))
+    temp.add_menu(id, name, url, selector, n=range(0,3))
 
     # Lavande Restaurant - Daily
+    id = 5
     name = "Lavande Restaurant - Daily"
     url = "https://restaurantlavande.cz/menu/#week-menu"
     weekday = datetime.today().weekday()
     weekday = weekday if weekday < 5 else 4
     n = (( weekday + 1) * 4) - 1
     selector = ".week-menu__header ~ .menus .menus__menu-content h3 ~ div .food__name"
-    temp.add_menu(name, url, selector, n=range(n,n+4))
+    temp.add_menu(id, name, url, selector, n=range(n,n+4))
 
 if __name__ == "__main__":
     x = lunchScraper()
     your_restaurants(x)
-    result = x.send_message()
+    result = x.send_messages()
     print("[{}] Execution completed with {}.".format(datetime.now(), result) )
