1a3e2154e713d108e681663a4c20356edae1b578
Vojtech Kotek <kotek.vojtech@gmail.com>
2019-07-14 20:57:31 +0200

    fixing requirements and adding info to setup

--- setup.py
+++ setup.py
@@ -1,3 +1,11 @@
 from setuptools import setup, find_packages
 
-setup(name="LUNCHSCRAPER", packages=find_packages())
+setup(
+    name="LUNCHSCRAPER", 
+    version="1.4.0",
+    description="Fetches and email daily menus for restaurants",
+    author="Vojtech Kotek",
+    author_email="vojtech@kotek.co",
+    url="https://web.kotek.co/lunch-scraper",
+    packages=find_packages()
+)
