5a8c1396b0791f8f1a7f8880e535c5f6318167a1
Vojtech Kotek <kotek.vojtech@gmail.com>
2019-09-10 16:27:17 +0200

    adding installation of requirements.txt

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 from setuptools import setup, find_packages
 
+import os
+thelibFolder = os.path.dirname(os.path.realpath(__file__))
+requirementPath = thelibFolder + '/requirements.txt'
+install_requires = []
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
+
 setup(
     name="LUNCHSCRAPER", 
     version="1.5.0",
@@ -7,5 +15,6 @@ setup(
     author="Vojtech Kotek",
     author_email="vojtech@kotek.co",
     url="https://web.kotek.co/lunch-scraper",
+    install_requires=install_requires,
     packages=find_packages()
 )
