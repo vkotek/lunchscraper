38e6d508b9f63b015727a07b01aa585da54f0a11
Vojtech Kotek <kotek.vojtech@gmail.com>
2019-09-10 13:40:21 +0200

    attempt to fix CI

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="LUNCHSCRAPER", 
-    version="1.4.0",
+    version="1.5.0",
     description="Fetches and email daily menus for restaurants",
     author="Vojtech Kotek",
     author_email="vojtech@kotek.co",
