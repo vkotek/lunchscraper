964d3eeeac7293332efea5f6fd0b5a64beb9d887
Vojtech Kotek <kotek.vojtech@gmail.com>
2019-09-10 17:18:59 +0200

    adding mock function for integration tests of Google Translate API

--- lunchscraper/translator.py
+++ lunchscraper/translator.py
@@ -1,30 +1,37 @@
 import os
 
-path = "/home/vojtech/ipython/key.json"
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = path
+if os.environ.get('CI') == 'True':
+    
+    def translate(text, source_language, target_language):
+        return text
+    
+else:
+    
+    path = "/home/vojtech/ipython/key.json"
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = path
 
-from google.cloud import translate_v3beta1 as translate
-client = translate.TranslationServiceClient()
+    from google.cloud import translate_v3beta1 as translate
+    client = translate.TranslationServiceClient()
 
-project_id = "lunch-scraper"
-location = "global"
+    project_id = "lunch-scraper"
+    location = "global"
 
-parent = client.location_path(project_id, location)
+    parent = client.location_path(project_id, location)
 
-def translate(text, source_language, target_language):
+    def translate(text, source_language, target_language):
 
-    if isinstance(text, list):
-        text = "\n".join(text)
+        if isinstance(text, list):
+            text = "\n".join(text)
 
-    response = client.translate_text(
-        parent=parent,
-        contents=[text],
-        mime_type='text/plain',  # mime types: text/plain, text/html
-        source_language_code=source_language,
-        target_language_code=target_language)
+        response = client.translate_text(
+            parent=parent,
+            contents=[text],
+            mime_type='text/plain',  # mime types: text/plain, text/html
+            source_language_code=source_language,
+            target_language_code=target_language)
 
-    try:
-        response = response.translations[0].translated_text
-        response = response.split("\n")
-    finally:
-        return response
+        try:
+            response = response.translations[0].translated_text
+            response = response.split("\n")
+        finally:
+            return response
